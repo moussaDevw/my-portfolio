8c4e576d96d0ea56755f79b9145c6064df4b270d
moussaDevw <musadevweb@gmail.com>
2024-02-19 14:18:14 +0100

    add exeperirnce

--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -14,6 +14,55 @@ const Experience = () => {
       className="w-full flex flex-col lgl:flex-row gap-10 lgl:gap-20"
     >
       <div>
+      <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
+          <p className="text-sm text-designColor tracking-[4px]">2021 - 2024</p>
+          <h2 className="text-3xl md:text-4xl font-bold">{t('jobExperience')}</h2>
+        </div>
+        <div className="mt-6 lgl:mt-14 w-full h-[1800px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <ResumeCard
+            title={t("frontEndDeveloper")}
+            subTitle={`Casacloud,Remote - (11-2022 - ${t('present')})`}
+            result={`${t('senegal')},Dakar`}
+            des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up
+            Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
+          />
+          <ResumeCard
+            title={t("fullStackDeveloper")}
+            subTitle="3W Agency - (11-2024 - 12-2024)"
+            result={`${t('senegal')},Dakar`}
+            des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
+          />
+          <ResumeCard
+            title={t("frontEndDeveloper")}
+            subTitle="Medteck Consulting - (02-2022 - 01-2023)"
+            result={`${t('senegal')},Dakar`}
+            des="The Oman economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
+          />
+          <ResumeCard
+            title={t("web/mobileDeveloper")}
+            subTitle="SenAgriculture - (04-2021 - 11-2021)"
+            result={`${t('senegal')},Dakar`}
+            des={<ul>
+              <li>- {t('senagricultureDesc.0')}</li>
+              <br />
+              <li>- {t('senagricultureDesc.1')}</li>
+              <br />
+              <li>- {t('senagricultureDesc.2')}</li>
+              <br />
+              <li>- {t('senagricultureDesc.3')}</li>
+              <br />
+              <li>- {t('senagricultureDesc.4')}</li>
+              <br />
+              <li>- {t('senagricultureDesc.5')}</li>
+              <br />
+              <li>- {t('senagricultureDesc.6')}</li>
+              <br />
+              <li>- {t('senagricultureDesc.7')}</li>
+            </ul>}
+          />
+        </div>
+      </div>
+      <div>
         <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
           <p className="text-sm text-designColor tracking-[4px]">2019 - 2021</p>
           <h2 className="text-3xl md:text-4xl font-bold">{t('internshipExperience')}</h2>
@@ -21,48 +70,27 @@ const Experience = () => {
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
             title={t("webDeveloper")}
+            subTitle="Société africaine de facilitation (01-02-2020 - 01-08-2020)"
+            result={`${t('senegal')},Dakar`}
+            des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up
+            Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
+          />
+           <ResumeCard
+            title={t("webDeveloper")}
             subTitle="Volkeno,Dakar - (01-03-2019 - 01-08-2019)"
-            result="USA"
-            des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
+            result={`${t('senegal')},Dakar`}
+            des={<ul>
+              <li>- {t('volkenoDesc.0')}</li>
+              <br />
+              <li>- {t('volkenoDesc.1')}</li>
+            </ul>}
           />
-          <ResumeCard
-            title="Web Developer & Trainer"
-            subTitle="Apple Developer Team - (2012 - 2016)"
-            result="MALAYSIA"
-            des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
-          />
-          <ResumeCard
-            title="Front-end Developer"
-            subTitle="Nike - (2020 - 2011)"
-            result="Oman"
-            des="The Oman economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
-          />
-        </div>
-      </div>
-      <div>
-      <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
-          <p className="text-sm text-designColor tracking-[4px]">2021 - 2024</p>
-          <h2 className="text-3xl md:text-4xl font-bold">{t('jobExperience')}</h2>
-        </div>
-        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
-          <ResumeCard
-            title="Gym Instructor"
-            subTitle="Rainbow Gym Center (2015 - 2020)"
-            result="DHAKA"
-            des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
-          />
-          <ResumeCard
-            title="Web Developer and Instructor"
-            subTitle="SuperKing College (2010 - 2014)"
-            result="CANADA"
-            des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
-          />
-          <ResumeCard
+          {/* <ResumeCard
             title="School Teacher"
             subTitle="Kingstar Secondary School (2001 - 2010)"
             result="NEVADA"
             des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
-          />
+          /> */}
         </div>
       </div>
     </motion.div>
